70639fea648b19322088a946d0402fe9ba930872
DESKTOP-87IG1V9\Pavel <pasha_khodor@mail.ru>
2024-05-29 16:44:13 +0300

    updated game

--- gallows-game/src/App.vue
+++ gallows-game/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div v-for="item in word">
-    <span>item</span>
-  </div>
   <AppHeader />
   <div class="game-container">
-    <AppGamePerson />
-    <AppWrongLetters />
-    <AppPlayingField :word="word" :latter="latter" />
+    <div>{{ letters }}</div>
+    <div> {{ wrongLetters }}</div>
+    <div> {{ correctLetters }} </div>
+    <AppGamePerson :wrong-letters="wrongLetters"/>
+    <AppWrongLetters :wrong-letters="wrongLetters" />
+    <AppPlayingField :word="word" :correct-letters="correctLetters" />
   </div>
 
-  <AppPopup v-if="false" />
+  <AppPopup v-if="endGame" />
 
   <!-- Notification -->
   <div class="notification-container show">
@@ -17,7 +17,7 @@
   </div>
 </template>
 <script setup lang="ts">
-import { defineComponent, ref, onMounted } from 'vue'
+import { ref, computed } from 'vue'
 import AppHeader from './components/AppHeader.vue'
 import AppGamePerson from './components/AppGamePerson.vue'
 import AppWrongLetters from './components/AppWrongLetters.vue'
@@ -25,9 +25,24 @@ import AppPlayingField from './components/AppPlayingField.vue'
 import AppPopup from './components/AppPopup.vue'
 
 const word = ref('арина')
-const latter = ref('а')
+const letters = ref<string[]>([])
+const correctLetters = computed(() => letters.value.filter(x => word.value.includes(x)))
+const wrongLetters = computed(() => letters.value.filter(x => !word.value.includes(x)))
+const endGame = computed(() => {
+  console.log(word.value, '111111111')
+  console.log(correctLetters.value, '22222222')
 
-onMounted(() => {
-  console.log('примонтирован!', word.value)
+  if (wrongLetters.value.length >= 6) {
+    return 'lose'
+  } if (word.value === correctLetters.value.join('')) {
+    return 'win'
+  }
+})
+
+
+window.addEventListener('keydown', ({ key }) => {
+  if (/[а-яА-ЯёЁ]/.test(key)) {
+    letters.value.push(key.toLowerCase())
+  }
 })
 </script>
--- gallows-game/src/components/AppPlayingField.vue
+++ gallows-game/src/components/AppPlayingField.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="word">
     <span v-for="(item, index) in word" :key="index" class="letter">
-      {{ item.includes(latter) ? latter : '' }}</span
+      {{ correctLetters.includes(item) ? item : '' }}</span
     >
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineComponent, ref, computed, onMounted } from 'vue'
 
 interface Props {
   word: string
-  latter: string
+  correctLetters: string[]
 }
 
 defineProps<Props>()
