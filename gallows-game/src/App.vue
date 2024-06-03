72ab77924be0a848fec9434329b4a75dd41dac33
Pasha <pasha_khodor@mail.ru>
2024-06-03 17:08:44 +0300

    added popup

--- gallows-game/src/App.vue
+++ gallows-game/src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <AppHeader />
   <div class="game-container">
-    <div>{{ letters }}</div>
-    <div>{{ wrongLetters }}</div>
-    <div>{{ correctLetters }}</div>
     <AppGamePerson :wrong-letters="wrongLetters" />
     <AppWrongLetters :wrong-letters="wrongLetters" />
     <AppPlayingField :word="word" :correct-letters="correctLetters" />
     <AppNotification ref="notification" />
   </div>
 
-  <AppPopup v-if="isWin || isLose" :result="popup" :word="word" />
+  <AppPopup :word="word" @restart="restart" ref="popup" />
 </template>
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue'
@@ -20,8 +17,21 @@ import AppWrongLetters from './components/AppWrongLetters.vue'
 import AppPlayingField from './components/AppPlayingField.vue'
 import AppPopup from './components/AppPopup.vue'
 import AppNotification from './components/AppNotification.vue'
+import { getRandomName } from './api/getRandomName'
 
 const word = ref('арина')
+const getRandomWord = async () => {
+  try {
+    const name = await getRandomName()
+    word.value = name.toLowerCase()
+  } catch (err) {
+    console.log(err)
+    word.value = ''
+  }
+}
+
+getRandomWord()
+
 const letters = ref<string[]>([])
 const correctLetters = computed(() => letters.value.filter((x) => word.value.includes(x)))
 const wrongLetters = computed(() => letters.value.filter((x) => !word.value.includes(x)))
@@ -29,21 +39,25 @@ const wrongLetters = computed(() => letters.value.filter((x) => !word.value.incl
 const isWin = computed(() => [...word.value].every((x) => correctLetters.value.includes(x)))
 const isLose = computed(() => wrongLetters.value.length >= 6)
 const notification = ref<InstanceType<typeof AppNotification> | null>(null)
-const popup: any = ref(null)
+const popup = ref<InstanceType<typeof AppPopup> | null>(null)
+// const popup: any = ref(null)
 
 watch(correctLetters, () => {
   if (isWin.value) {
-    popup.value = 'win'
+    popup.value?.open('win')
   }
 })
 
 watch(wrongLetters, () => {
   if (isLose.value) {
-    popup.value = 'lose'
+    popup.value?.open('lose')
   }
 })
 
 window.addEventListener('keydown', ({ key }) => {
+  if (isLose.value || isWin.value) {
+    return
+  }
   if (letters.value.includes(key)) {
     notification.value?.open()
     setTimeout(() => {
@@ -55,4 +69,10 @@ window.addEventListener('keydown', ({ key }) => {
     letters.value.push(key.toLowerCase())
   }
 })
+
+const restart = async () => {
+  await getRandomWord()
+  letters.value = []
+  popup.value?.close()
+}
 </script>
--- gallows-game/src/api/getRandomName.ts
+++ gallows-game/src/api/getRandomName.ts
@@ -1,1 +1,9 @@
-'https://api.randomdatatools.ru/?unescaped=false&params=FirstName'
+// 'https://api.randomdatatools.ru/?unescaped=false&params=FirstName'
+import axios from 'axios'
+
+export const getRandomName = async () => {
+  const { data } = await axios<{ FirstName: String }>(
+    'https://api.randomdatatools.ru/?unescaped=false&params=FirstName'
+  )
+  return data.FirstName
+}
